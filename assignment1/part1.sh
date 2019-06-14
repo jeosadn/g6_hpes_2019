478054822ab8ce432a01ce051ea5ada627c5b139
Jose Pacheco <jap@hpe.com>
2019-06-14 15:34:07 -0700

    More progress

--- assignment1/part1.sh
+++ assignment1/part1.sh
@@ -10,7 +10,7 @@ fi
 
 #All dependencies install
 if false; then
-   sudo apt-get install 'g++-arm-linux-gnueabihf' qemu-system-arm libncurses-dev flex bison libssl-dev
+   sudo apt-get install 'g++-arm-linux-gnueabihf' qemu-system-arm libncurses-dev flex bison libssl-dev device-tree-compiler
 fi
 
 #Compiling uboot
@@ -19,7 +19,9 @@ if false; then
    cd u-boot-2016.11
    make vexpress-ca9x4-defconfig
    make all CROSS_COMPILE=arm-linux-gnueabihf-
-   qemu-system-arm -machine vexpress-a9 -nographic -no-reboot -kernel u-boot #This command is interactive
+   cp u-boot ../.
+   cd ..
+   qemu-system-arm -machine vexpress-a9 -nographic -no-reboot -kernel u-boot
    #Looks like uboot is compiled and qemu can run it at this point.
 fi
 
@@ -28,7 +30,16 @@ if false; then
    tar xf v5.1.tar.gz
    cd linux-5.1
    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig #This step is interactive, configure whatever
-   make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all
-   #At this point compilation ran for ~2 hours and then failed
+   make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all #Compilation takes ~2hours, fails in stage2 but zImage is done
+   cp arch/arm/boot/zImage ../.
+
+   #Due to the error, hand-compile the dtb file
+   sed -i 's/#include/\/include\//' ./arch/arm/boot/dts/vexpress-v2p-ca9.dts
+   dtc -O dtb -o ../vexpress-v2p-ca9.dtb ./arch/arm/boot/dts/vexpress-v2p-ca9.dts
+   cd ../
+
+   #This QEMU command stalls
+   qemu-system-arm -machine vexpress-a9 -nographic -no-reboot -kernel zImage -dtb vexpress-v2p-ca9.dtb -cpu cortex-a9 -m 512M
+   qemu-system-arm -machine vexpress-a9 -nographic            -kernel zImage -dtb vexpress-v2p-ca9.dtb -cpu cortex-a9 -m 512M
 fi
 
